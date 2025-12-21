e298d72d33514e0565a8b510c4f23fa50c5dbe0f
Rishi Prakash Mohapatra <mohapatrarishiprakash@gmail.com>
2025-12-21 23:03:21 +0530

    Add files via upload

---
+++ day_03/05_operators.py
@@ -0,0 +1,42 @@
+# arithmatic operator 
+
+print(7+2)  # adition
+print(7-2)  # substraction
+print(7*2)  # multiplication
+print(7/2)  # division
+print(10//3) # floor division
+
+# Assignment operator
+
+a = 5 
+print(a)
+a += 3         #increment a by 3
+print(a)
+a -= 4         # decrement a by 4 
+print(a)
+a *= 5         # multiply a by 5 
+print(a)
+a /= 2         #divide it by 2
+print(a)
+
+
+# Comparison operator   
+
+a = 10
+b = 5
+
+print(a == b)   # Equal to
+print(a != b)   # Not equal to
+print(a > b)    # Greater than
+print(a < b)    # Less than
+print(a >= b)   # Greater than or equal to
+print(a <= b)   # Less than or equal to
+
+# Logical operators
+ 
+a = 10
+b = 5
+
+print(a > 5 and b > 2)   # and (both conditions must be True)
+print(a > 5 or b > 10)  # or (at least one condition True)
+print(not a > 5)        # not (reverses the result)
